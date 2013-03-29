47344cecb160dc663a9789f4b9851c3344a91b22
Victor Costan <costan@gmail.com>
2013-03-29 02:11:52 -0400

    Code for pushing readings to the server.

--- src/edu/mit/csail/netmap/sensors/Recorder.java
+++ src/edu/mit/csail/netmap/sensors/Recorder.java
@@ -1,11 +1,251 @@
 package edu.mit.csail.netmap.sensors;
 
-import android.content.Context;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
-/** Accumulates sensor readings so they can be uploaded over WiFi. */
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.http.AndroidHttpClient;
+import android.util.Log;
+
+/** Queues up sensor readings so they can be uploaded over WiFi. */
 public class Recorder {
+  /** Approximate size of a pack of readings. */
+  private static final int PACK_SIZE = 128 * 1024;
+  
+  /** User-Agent header used during uploading. */
+  private static final String USER_AGENT = "NetMap/1.0 Recorder/1.0";
+  
+  /** Logging tag. */
+  private static final String TAG = "recorder";
+  
+  /** The table that recordings are saved into. */
+  private static final String TABLE_NAME = "metrics";
+  
+  /** Android context used to access the application's private storage. */
+  private static Context context_ = null;
+  
+  /** The database that stores sensor readings. */
+  private static SQLiteDatabase db_ = null;
+  
+  /** Message digest cloned for each reading. */
+  private static MessageDigest digestPrototype_ = null;
+  
+  /**
+   * Adds a sensor reading to the transmission queue.
+   * 
+   * @param jsonData the reading's information, packed as a JSON string
+   * @return a crypto hash of the reading 
+   */
+  public static String storeReading(String jsonData) {
+    String digest = Recorder.digest(jsonData);
+    insertReading(jsonData);  
+    return digest;
+  }
+  
+  /**
+   * 
+   * 
+   * @param uploadUri {@link URI} for the HTTP backend that receives sensor data
+   * @return if true, the caller should call {@link #sendPack(URI)} again to
+   *     upload more data 
+   */
+  public static boolean sendPack(URI uploadUri) {
+    StringBuffer packData = new StringBuffer();
+    long lastReadingId = readPack(packData, PACK_SIZE);
+    if (lastReadingId == 0) {
+      // No stored readings.
+      return false;
+    }
+    if (!uploadPack(packData.toString(), uploadUri)) {
+      // The data upload failed.
+      return true;
+    }
+    deletePack(lastReadingId);
+    return true;
+  }
+
   /** Called by {@link Sensors#initialize(android.content.Context)}. */
   public static void initialize(Context context) {
+    context_ = context;
     
+    DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);    
+    db_ = databaseOpenHelper.getWritableDatabase();
+    
+    try {
+      digestPrototype_ = MessageDigest.getInstance("SHA-1");
+    } catch (NoSuchAlgorithmException e) {
+      Log.e(TAG, "Device does not support SHA-1");
+    }
+  }
+  
+  /**
+   * Fetches sensor readings from the database, so they can be uploaded.
+   * 
+   * @param packData {@link StringBuffer} that receives readings, in a format
+   *     suitable for server consumption
+   * @param packSize a guideline for the size of JSON data to be read
+   * @return the ID of the last reading 
+   */
+  private static final long readPack(StringBuffer packData, int packSize) {
+    long lastReadingId = 0;  // INTEGER PRIMARY KEY values start at 1
+    int limit = 100;
+    
+    while (true) {
+      final Cursor cursor = db_.rawQuery("SELECT id, json FROM " + TABLE_NAME +
+          " WHERE id > " + Long.toString(lastReadingId) +
+          " ORDER BY id LIMIT " + Integer.toString(limit), null);
+      if (!cursor.moveToFirst()) {
+        // Empty result set.
+        break;
+      }
+      while (true) {
+        lastReadingId = cursor.getLong(0);
+        final String jsonData = cursor.getString(1);
+        appendReading(packData, jsonData);
+        if (packData.length() >= packSize) {
+          // Got enough data.
+          cursor.close();
+          return lastReadingId;
+        }
+        
+        if (!cursor.moveToNext()) {
+          // Done with this query set.
+          cursor.close();
+          break;
+        }
+      }
+    }
+    return lastReadingId;
+  }
+  
+  /**
+   * Uploads a pack of sensor readings data to the server.
+   * 
+   * @param packData 
+   * @param uploadUri {@link URI} for the HTTP backend that receives sensor data
+   * @return
+   */
+  private static final boolean uploadPack(String packData, URI uploadUri) {
+    HttpPost request = new HttpPost(uploadUri);
+    try {
+      request.setEntity(new StringEntity(packData));
+    } catch (UnsupportedEncodingException e) {
+      Log.e(TAG, "http.entity.StringEntity rejected pack");
+      return false;
+    }
+    HttpClient httpClient = AndroidHttpClient.newInstance(USER_AGENT, context_);
+    try {
+      httpClient.execute(request);
+      return true;
+    } catch (ClientProtocolException e) {
+      Log.e(TAG, "ClientProtocolException during pack upload");
+      return false;
+    } catch (IOException e) {
+      Log.e(TAG, "IOException during pack upload");
+      return false;
+    }
+  }
+  
+  /**
+   * Removes sensor readings that were uploaded from the database.
+   * 
+   * This method permanently removes the readings from the local database, so it
+   * should only be called 
+   * 
+   * @param lastReadingId return value from {@link #readPack(ArrayList, int)}
+   */
+  private static final void deletePack(long lastReadingId) {
+    if (lastReadingId == 0) {
+      // readPack did not return any data
+      return;
+    }
+    db_.execSQL("DELETE FROM  " + TABLE_NAME + " WHERE id <= " +
+        Long.toString(lastReadingId));
+  }
+
+  /**
+   * Adds a reading's data to a pack that will be uploaded to the server.
+   * 
+   * @param packData {@link StringBuffer} that receives readings, in a format
+   *     suitable for server consumption
+   * @param jsonData a sensor reading's data, encoded as a JSON string
+   */
+  private static final void appendReading(StringBuffer packData,
+      String jsonData) {
+    packData.append(jsonData);
+    packData.append('\n');
+  }
+  
+  /**
+   * 
+   * 
+   * @param jsonData the reading's information, packed as a JSON string
+   */
+  private static final void insertReading(String jsonData) {
+    ContentValues values = new ContentValues();
+    values.put("json", jsonData);
+    db_.insert(TABLE_NAME, null, values);
+  }
+  
+  /**
+   * Computes a cryptographically secure hash of a reading's data.
+   * 
+   * @param jsonData the reading's information, packed as JSON string
+   * @return a small string that uniquely identifies this reading
+   */
+  private static final String digest(String jsonData) {
+    MessageDigest digest;
+    try {
+      digest = (MessageDigest)digestPrototype_.clone();
+    } catch(CloneNotSupportedException e) {
+      try {
+        digest = MessageDigest.getInstance("SHA-1");
+      } catch (NoSuchAlgorithmException e1) {
+        Log.e(TAG, "Device does not support SHA-1");
+        return null;
+      }
+    }
+    byte[] digestBytes = digest.digest(jsonData.getBytes(Charset.defaultCharset()));
+    StringBuffer hexDigest = new StringBuffer();
+    for (byte b : digestBytes) {
+      String hexByte = Integer.toHexString(b);
+      if (hexByte.length() != 2) {  // hexByte will have 1 or 2 hex digits
+        hexDigest.append('0');
+      }
+      hexDigest.append(hexByte);
+    }
+    return hexDigest.toString();
+  }  
+
+  /** Opens the readings database and creates its schema. */
+  private static class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public DatabaseOpenHelper(Context context) {
+      super(context, "metrics", null, 1);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+      // We'll fill this in if the database ever requires schema changes.
+    }
+    
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+      db.execSQL("CREATE TABLE " + TABLE_NAME +
+          " (id INTEGER PRIMARY KEY, json TEXT);");
+    }    
   }
 }
